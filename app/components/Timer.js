6b60f164c6d4a43f21203ae5e6ed789d0de13cc6
Raul Barboza <ruzito@gmail.com>
2019-08-06 20:32:00 -0300

    updates moment

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import moment from 'moment';
 
 function Timer(props) {
     return(
         <>
             {props.tasks.map((task) => {
-                return <div key={task.id}>{task.start}</div>
+                const a = moment.unix(task.start).toDate();
+                const x = moment(a)
+                const b = moment.unix(task.stop).toDate();
+                const y = moment(b)
+                //const result = b.diff(a)
+                console.log(y.diff(x, 'minutes'))
+                return <div key={task.id}>{} {}</div>
             })}
         </>
     )
